89df681fa825d9405299186fd926ae1f5727eeca
Mattias Ekendahl <mattias@developedbyme.com>
2024-08-13 14:36:54 +0200

    Update: using live data

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
 <?php
-  header('Content-type: text/csv');
+  
 
-  
-  
+  //METODO: get data from api
+  $ch = curl_init();
+
+$url = "https://wei23s-3epartsintegration.vercel.app/";
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// Set the option to return the response as a string instead of outputting it directly
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$headers = [
+    "Content-Type: application/json"
+];
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$response = curl_exec($ch);
+
+if (curl_errno($ch)) {
+  header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+    echo 'cURL Error: ' . curl_error($ch);
+} else {
+ header('Content-type: text/csv');
+
+  $data = json_decode($response, true);
+
   echo("id,name\n");
-  echo("1,Mattias\n");
-  echo("2,Linus\n");
+
+  foreach($data as $row) {
+    echo($row['id'].",".$row['name']."\n");
+  }
+}
+
+curl_close($ch);
 ?>
